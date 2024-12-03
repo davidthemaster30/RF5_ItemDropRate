fcf8c1afeab73a4326fd5039868ad5107cdd78b1
David <davidthemaster30@hotmail.com>
2024-12-02 21:57:46 -0500

    change defaults

--- RF5ItemDropRate.cs
+++ RF5ItemDropRate.cs
@@ -22,11 +22,11 @@ public class RF5ItemDropRate : BasePlugin
 
     internal static void LoadConfig(ConfigFile Config)
     {
-        ItemDropRatePatch.MonsterItemDrops = Config.Bind(PluginConfigSection, nameof(ItemDropRatePatch.MonsterItemDrops), 5, new ConfigDescription("Base monster item drop rate multiplier.", new AcceptableValueRange<int>(1, 50)));
+        ItemDropRatePatch.MonsterItemDrops = Config.Bind(PluginConfigSection, nameof(ItemDropRatePatch.MonsterItemDrops), 5, new ConfigDescription("Base monster item drop rate multiplier.", new AcceptableValueRange<int>(1, 1000)));
         ItemDropRatePatch.MonsterItemDrops.SettingChanged += ItemDropRatePatch.OnSettingChanged;
-        ItemDropRatePatch.MonsterBonusItemDrops = Config.Bind(PluginConfigSection, nameof(ItemDropRatePatch.MonsterBonusItemDrops), 5, new ConfigDescription("Bonus monster item drop rate multiplier.", new AcceptableValueRange<int>(1, 50)));
+        ItemDropRatePatch.MonsterBonusItemDrops = Config.Bind(PluginConfigSection, nameof(ItemDropRatePatch.MonsterBonusItemDrops), 5, new ConfigDescription("Bonus monster item drop rate multiplier.", new AcceptableValueRange<int>(1, 1000)));
         ItemDropRatePatch.MonsterBonusItemDrops.SettingChanged += ItemDropRatePatch.OnSettingChanged;
-        ItemDropRatePatch.MonsterSealItemDrops = Config.Bind(PluginConfigSection, nameof(ItemDropRatePatch.MonsterSealItemDrops), 5, new ConfigDescription("Sealed monster item drop rate multiplier.", new AcceptableValueRange<int>(1, 50)));
+        ItemDropRatePatch.MonsterSealItemDrops = Config.Bind(PluginConfigSection, nameof(ItemDropRatePatch.MonsterSealItemDrops), 5, new ConfigDescription("Sealed monster item drop rate multiplier.", new AcceptableValueRange<int>(1, 1000)));
         ItemDropRatePatch.MonsterSealItemDrops.SettingChanged += ItemDropRatePatch.OnSettingChanged;
     }
 
